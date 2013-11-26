0783f719ff8f6b0718f1dca63503fd8525f45daa
bj22dv <bj22dv@student.lnu.se>
2013-11-26 14:53:38 +0100

    Hemliga talet klart

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,14 +2,32 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+    var secret = Math.floor(Math.random() * 100) + 1;
+    var guesses = 0;
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 			
-		// Plats för förändring.
+		if (isNaN(number) || Math.floor(+number) !=number) {
+		    return [false, "Vänligen ange ett heltal."];
+		}
+		else if (0 > number || number > 100) {
+		    return [false, "Talet är utanför intervallet 0 - 100."];
+		}
+
+		guesses += 1;
+
+		if (number == secret) {
+		    return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + guesses + " gissningar för att hitta det."];
+		}
+		else if (number > secret) {
+		    return [false, "Det hemliga talet är lägre!"];
+		}
+		else {
+		    return [false, "Det hemliga talet är högre!"];
+		}
 
 
 		// Returnera exempelvis: 
